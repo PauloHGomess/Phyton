15d6508ab6d7c78f7420fb190633247a3dbb2cb0
Paulo H Gomes <162829112+PauloHGomess@users.noreply.github.com>
2024-07-22 11:39:55 -0300

    Create revisao.py

---
+++ revisao.py
@@ -0,0 +1,34 @@
+##revisao
+'01'
+num1= int(input("Por obsequio digite o numero 1"))
+num2= int (input("por favor digite o numero 2"))
+soma= num1+num2
+print(f"a soma dos dois numeros é: {soma}")
+'02'
+pi= 3.14;
+raio= float(input("Digite o raio"))
+area= (pi*raio**2)
+print (f"p valor da area é: {area}")
+'03'
+hr= float(input("Digite o valor da hora trabalhada"))
+nhr= float(input("Digite a quantidade mensal de horas trabalhadas"))
+perc= float(input("qual o percentual de desconto do inss"))
+bruto= hr*nhr;
+desc= (bruto*perc/100);
+liq= bruto-desc;
+print(f"o salario liquido é:{liq}")
+'04'
+nome= (input("Digite o seu nome "))
+sexo= (input("Digite o sexo"))
+print(f" o nome da pessoa é {nome} e o seu sexo {sexo}!")
+'05'
+a= float(input("numero 1"))
+b= float(input("numero 2"))
+div= a/b
+total= div**2;
+print(f"o valor é {total}")
+'06'
+a= int(input("digite um numero"))
+suc= a+1 
+ant=a-1
+print(f" o numero é: {a}\n o seu antecessor é: {ant} \n o seu sucessor é: {suc}")
